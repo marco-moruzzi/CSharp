84855a54f14c7f37591ff688060059a60053a45d
marco-moruzzi <77643565+marco-moruzzi@users.noreply.github.com>
2021-09-16 14:59:24 +0200

    Add files via upload

---
+++ Conversione/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = 10;
+double y = x;
+float x1 = 12000f;
+int y1 = (int) x1;
+Console.WriteLine(x1);
+Console.WriteLine(y1);
+int y2 = (int) x1++;
+Console.WriteLine(y2);
+string s = "12345";
+int x3 = Convert.ToInt32(s);
+int x4 = x3 + 1;
+Console.WriteLine(x4);
+Console.Write("Come ti chiami? ");
+string nome = Console.ReadLine();
+Console.WriteLine(nome);
+//int[,] tabella = new int[5,5];
+/*int[][] tabella = new int[5][];
+for (int i = 0; i < 5; i++) {
+    int z = 0;
+    tabella[i][i] = z;
+    for (int j = 1; j < 5; j++) {
+        tabella[i][j] = z + 1;
+        Console.Write(tabella[i][j]);
+    }
+}*/
+int[,] arr = new int[5,5];
+int counter = 0;
+for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+        ++counter;
+        arr[i,j] = (j == 0) ? 0 : counter;
+    }
+}
+for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+        Console.Write($"{arr[i,j]} ");
+    }
+    Console.Write("\n");
+}
---
+++ TypeInference/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*float f = 1.25f;
+double d = 130.78;*/
+/*decimal dm = 1500.456m;
+double ne = 5e-7;
+var s = "type inference";*/
+bool b = false;
+Console.WriteLine($"{b}");
