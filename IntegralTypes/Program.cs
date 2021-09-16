7c8d26bedf804d3ed57f9b292b22285a505a5d2e
marco-moruzzi <77643565+marco-moruzzi@users.noreply.github.com>
2021-09-16 14:56:44 +0200

    Add files via upload

---
+++ IntegralTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int a = 500;
+short b = 100;
+sbyte c = -50;
+long d = 1_000_000;
+int e = 0b10101010;
+ushort f = 0x0A56;
+Console.WriteLine("Integer: {0}, Short: {1}, Unsigned Byte: {2}, Long: {3}, Binary Integer: {4}, Unsigned Hexadecimal Short: {5}.",a,b,c,d,e,f);
+Console.WriteLine($"Integer: {a} \n Short: {b} \n Unsigned Byte: {c} \n Long: {d} \n Binary Integer: {e} \n Unsigned Hexadecimal Short: {f}.");
