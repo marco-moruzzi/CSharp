703b3546403b1a4c55daad0ab67e63e7ed98c44c
marco-moruzzi <77643565+marco-moruzzi@users.noreply.github.com>
2021-09-17 15:15:05 +0200

    Generics modificato

---
+++ Generics/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class MyClass {
+    public T myMethod<T>(T v) {
+        return v;
+    }
+}
+//MyClass c = new MyClass();
+//c.myMethod("Mario");
