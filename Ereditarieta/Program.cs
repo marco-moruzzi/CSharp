81948f8db400759dd1c191ba344c2820322baf1c
marco-moruzzi <77643565+marco-moruzzi@users.noreply.github.com>
2021-09-17 14:32:47 +0200

    Abbozzo esercizio ereditarieta

---
+++ Ereditarieta/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Persona {
+    public string nome { get; set;}
+    public string cognome { get; set;}
+    public virtual string Denominazione {
+        return nome + " " + cognome;
+    }
+}
+public class Utente : Persona {
+    public int id { get; set;}
+    public int annoIscrizione { get; set;}
+    public override string Denominazione {
+        return id + " " + nome + " " + cognome;
+    }
+}
+Console.WriteLine("Hello, World!");
+
